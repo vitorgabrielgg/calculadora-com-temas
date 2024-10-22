5ab5d664f213dbb3eb04550034c78ad4c574320e
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-10-22 10:49:42 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Calculator } from "./page/Calculator";
+import { GlobalStyle } from "./styles/globalStyle";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
