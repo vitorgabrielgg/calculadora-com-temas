b8ac2259f80f0e410b18826dff9131d2e9e00eae
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-10-26 11:11:07 -0300

    style: add global class for color transition

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -7,4 +7,10 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         font-family: "League Spartan", sans-serif;
     }
+
+    .color_transition{
+        transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 400ms;
+    }
 `;
