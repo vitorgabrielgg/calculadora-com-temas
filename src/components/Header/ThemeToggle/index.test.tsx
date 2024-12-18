b42a736089a2bb2a8536f2f6813aeea14dfc74ac
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-12-18 18:31:49 -0300

    test: corrects theme toggle tests

--- src/components/Header/ThemeToggle/index.test.tsx
+++ src/components/Header/ThemeToggle/index.test.tsx
@@ -1,41 +1,46 @@
-import { render, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
-import { ThemeToggle } from ".";
+import { act } from "react";
+import App from "../../../App";
 
 describe("Change Theme", () => {
   it("should show the toggle item of theme 1", async () => {
-    const { getByLabelText } = render(<ThemeToggle />);
+    renderApp();
 
-    const toggleItem1 = getByLabelText("toggle item 1");
+    const toggleItem1 = screen.getByLabelText("toggle item 1");
 
-    userEvent.click(toggleItem1);
-
-    waitFor(() => {
-      expect(toggleItem1).toHaveClass("opacity_100");
+    await act(async () => {
+      await userEvent.click(toggleItem1);
     });
+
+    expect(toggleItem1).toHaveClass("opacity_100");
   });
 
-  it("should show the toggle item of theme 2", () => {
-    const { getByLabelText } = render(<ThemeToggle />);
+  it("should show the toggle item of theme 2", async () => {
+    renderApp();
 
-    const toggleItem2 = getByLabelText("toggle item 2");
+    const toggleItem2 = screen.getByLabelText("toggle item 2");
 
-    userEvent.click(toggleItem2);
-
-    waitFor(() => {
-      expect(toggleItem2).toHaveClass("opacity_100");
+    await act(async () => {
+      await userEvent.click(toggleItem2);
     });
+
+    expect(toggleItem2).toHaveClass("opacity_100");
   });
 
   it("should show the toggle item of theme 3", async () => {
-    const { getByLabelText } = render(<ThemeToggle />);
+    renderApp();
 
-    const toggleItem3 = getByLabelText("toggle item 3");
+    const toggleItem3 = screen.getByLabelText("toggle item 3");
 
-    await userEvent.click(toggleItem3);
-
-    waitFor(() => {
-      expect(toggleItem3).toHaveClass("opacity_100");
+    await act(async () => {
+      await userEvent.click(toggleItem3);
     });
+
+    expect(toggleItem3).toHaveClass("opacity_100");
   });
 });
+
+const renderApp = () => {
+  render(<App />);
+};
